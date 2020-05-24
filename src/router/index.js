c633e2f1b3f7009b2b9c6a9e0234f47332893fa4
Jkaylight <jkaylight@gmail.com>
2020-05-24 21:27:19 +0100

    more fixes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+
 
 Vue.use(VueRouter);
 
@@ -14,11 +16,13 @@ const routes = [
   {
     path: "/about",
     name: "about",
+    component: About,
+    props: true
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    // component: () =>
+    //   import(/* webpackChunkName: "about" */ "../views/About.vue")
   }
 ];
 
