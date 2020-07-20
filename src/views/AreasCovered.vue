0e46d92ef5840be23e7ad06be2184ee1a5ff1c79
Jkaylight <jkaylight@gmail.com>
2020-07-21 00:27:40 +0100

    fixed in AreasCovered and Contact Page

---
+++ src/views/AreasCovered.vue
@@ -0,0 +1,343 @@
+<template>
+  <div>
+    <div class="section-1 m-0">
+      <div class="container ">
+        <div class="row d-flex">
+          <div
+            class="col-12 col-md-6 text-left justify-content-center align-self-center"
+          >
+            <div class="">
+              <h2 class="display-5 text-white mt-0">
+                Areas where we operate
+              </h2>
+              <p class="text-white text-left">
+                You can begin your driving lessions with just $245 in any of the
+                areas we cover in the United Kingdom
+              </p>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 pt-0">
+            <img
+              src="../assets/illustration.png"
+              alt="cars"
+              class="img-fluid"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!------------- Section-1 ------------->
+    <section class="area">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-12 text-center">
+            <div class="card mr-2 d-inline-block shadow-lg">
+              <div class="card-img-top">
+                <!-- <img src="../assets/info-bar-countries.svg" class="img-fluid border-radius p-4" alt="Member">  -->
+                <!-- <i class="fas fa-tachometer-alt" style="height: 200px; width: 200px;"></i> -->
+              </div>
+              <div class="card-body area-card">
+                <h3 class="card-title">Greater London</h3>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis consectetur quas, provident sit quibusdam similique,
+                  impedit aperiam in quidem ratione voluptatem
+                </p>
+                <a href="#" class="text-decoration-none">See More</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-12 text-center">
+            <div class="card mr-2 d-inline-block shadow-lg">
+              <div class="card-img-top">
+                <!-- <img src="../assets/info-bar-countries.svg" class="img-fluid border-radius p-4" alt="Member">  -->
+                <!-- <i class="fas fa-tachometer-alt" style="height: 200px; width: 200px;"></i> -->
+              </div>
+              <div class="card-body">
+                <h3 class="card-title">Kent</h3>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis consectetur quas, provident sit quibusdam similique,
+                  impedit aperiam in quidem ratione voluptatem
+                </p>
+                <a href="#" class="text-decoration-none">See More</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-12 text-center">
+            <div class="card mr-2 d-inline-block shadow-lg">
+              <div class="card-img-top">
+                <!-- <img src="../assets/info-bar-countries.svg" class="img-fluid border-radius p-4" alt="Member">  -->
+                <!-- <i class="fas fa-tachometer-alt" style="height: 200px; width: 200px;"></i> -->
+              </div>
+              <div class="card-body">
+                <h3 class="card-title">Hertfordshire</h3>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis consectetur quas, provident sit quibusdam similique,
+                  impedit aperiam in quidem ratione voluptatem
+                </p>
+                <a href="#" class="text-decoration-none">See More</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-12 text-center">
+            <div class="card mr-2 d-inline-block shadow-lg">
+              <div class="card-img-top">
+                <!-- <img src="../assets/info-bar-countries.svg" class="img-fluid border-radius p-4" alt="Member">  -->
+                <!-- <i class="fas fa-tachometer-alt" style="height: 200px; width: 200px;"></i> -->
+              </div>
+              <div class="card-body">
+                <h3 class="card-title">Merseyside</h3>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis consectetur quas, provident sit quibusdam similique,
+                  impedit aperiam in quidem ratione voluptatem
+                </p>
+                <a href="#" class="text-decoration-none">See More</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-12 text-center">
+            <div class="card mr-2 d-inline-block shadow-lg">
+              <div class="card-img-top">
+                <!-- <img src="../assets/info-bar-countries.svg" class="img-fluid border-radius p-4" alt="Member">  -->
+                <!-- <i class="fas fa-tachometer-alt" style="height: 200px; width: 200px;"></i> -->
+              </div>
+              <div class="card-body">
+                <h3 class="card-title">West Yorkshire</h3>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis consectetur quas, provident sit quibusdam similique,
+                  impedit aperiam in quidem ratione voluptatem
+                </p>
+                <a href="#" class="text-decoration-none">See More</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-12 text-center">
+            <div class="card mr-2 d-inline-block shadow-lg">
+              <div class="card-img-top">
+                <!-- <img src="../assets/info-bar-countries.svg" class="img-fluid border-radius p-4" alt="Member">  -->
+                <!-- <i class="fas fa-tachometer-alt" style="height: 200px; width: 200px;"></i> -->
+              </div>
+              <div class="card-body">
+                <h3 class="card-title">Hertfordshire</h3>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis consectetur quas, provident sit quibusdam similique,
+                  impedit aperiam in quidem ratione voluptatem
+                </p>
+                <a href="#" class="text-decoration-none">See More</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-12 text-center">
+            <div class="card mr-2 d-inline-block shadow-lg">
+              <div class="card-img-top">
+                <!-- <img src="../assets/info-bar-countries.svg" class="img-fluid border-radius p-4" alt="Member">  -->
+                <!-- <i class="fas fa-tachometer-alt" style="height: 200px; width: 200px;"></i> -->
+              </div>
+              <div class="card-body">
+                <h3 class="card-title">Merseyside</h3>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis consectetur quas, provident sit quibusdam similique,
+                  impedit aperiam in quidem ratione voluptatem
+                </p>
+                <a href="#" class="text-decoration-none">See More</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-12 text-center">
+            <div class="card mr-2 d-inline-block shadow-lg">
+              <div class="card-img-top">
+                <!-- <img src="../assets/info-bar-countries.svg" class="img-fluid border-radius p-4" alt="Member">  -->
+                <!-- <i class="fas fa-tachometer-alt" style="height: 200px; width: 200px;"></i> -->
+              </div>
+              <div class="card-body">
+                <h3 class="card-title">West Yorkshire</h3>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis consectetur quas, provident sit quibusdam similique,
+                  impedit aperiam in quidem ratione voluptatem
+                </p>
+                <a href="#" class="text-decoration-none">See More</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-12 text-center">
+            <div class="card mr-2 d-inline-block shadow-lg">
+              <div class="card-img-top">
+                <!-- <img src="../assets/info-bar-countries.svg" class="img-fluid border-radius p-4" alt="Member">  -->
+                <!-- <i class="fas fa-tachometer-alt" style="height: 200px; width: 200px;"></i> -->
+              </div>
+              <div class="card-body area-card">
+                <h3 class="card-title">Greater London</h3>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis consectetur quas, provident sit quibusdam similique,
+                  impedit aperiam in quidem ratione voluptatem
+                </p>
+                <a href="#" class="text-decoration-none">See More</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-12 text-center">
+            <div class="card mr-2 d-inline-block shadow-lg">
+              <div class="card-img-top">
+                <!-- <img src="../assets/info-bar-countries.svg" class="img-fluid border-radius p-4" alt="Member">  -->
+                <!-- <i class="fas fa-tachometer-alt" style="height: 200px; width: 200px;"></i> -->
+              </div>
+              <div class="card-body">
+                <h3 class="card-title">Kent</h3>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis consectetur quas, provident sit quibusdam similique,
+                  impedit aperiam in quidem ratione voluptatem
+                </p>
+                <a href="#" class="text-decoration-none">See More</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-12 text-center">
+            <div class="card mr-2 d-inline-block shadow-lg">
+              <div class="card-img-top">
+                <!-- <img src="../assets/info-bar-countries.svg" class="img-fluid border-radius p-4" alt="Member">  -->
+                <!-- <i class="fas fa-tachometer-alt" style="height: 200px; width: 200px;"></i> -->
+              </div>
+              <div class="card-body">
+                <h3 class="card-title">Hertfordshire</h3>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis consectetur quas, provident sit quibusdam similique,
+                  impedit aperiam in quidem ratione voluptatem
+                </p>
+                <a href="#" class="text-decoration-none">See More</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-12 text-center">
+            <div class="card mr-2 d-inline-block shadow-lg">
+              <div class="card-img-top">
+                <!-- <img src="../assets/info-bar-countries.svg" class="img-fluid border-radius p-4" alt="Member">  -->
+                <!-- <i class="fas fa-tachometer-alt" style="height: 200px; width: 200px;"></i> -->
+              </div>
+              <div class="card-body">
+                <h3 class="card-title">Merseyside</h3>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis consectetur quas, provident sit quibusdam similique,
+                  impedit aperiam in quidem ratione voluptatem
+                </p>
+                <a href="#" class="text-decoration-none">See More</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-12 text-center">
+            <div class="card mr-2 d-inline-block shadow-lg">
+              <div class="card-img-top">
+                <!-- <img src="../assets/info-bar-countries.svg" class="img-fluid border-radius p-4" alt="Member">  -->
+                <!-- <i class="fas fa-tachometer-alt" style="height: 200px; width: 200px;"></i> -->
+              </div>
+              <div class="card-body">
+                <h3 class="card-title">Merseyside</h3>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis consectetur quas, provident sit quibusdam similique,
+                  impedit aperiam in quidem ratione voluptatem
+                </p>
+                <a href="#" class="text-decoration-none">See More</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-12 text-center">
+            <div class="card mr-2 d-inline-block shadow-lg">
+              <div class="card-img-top">
+                <!-- <img src="../assets/info-bar-countries.svg" class="img-fluid border-radius p-4" alt="Member">  -->
+                <!-- <i class="fas fa-tachometer-alt" style="height: 200px; width: 200px;"></i> -->
+              </div>
+              <div class="card-body">
+                <h3 class="card-title">West Yorkshire</h3>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis consectetur quas, provident sit quibusdam similique,
+                  impedit aperiam in quidem ratione voluptatem
+                </p>
+                <a href="#" class="text-decoration-none">See More</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-12 text-center">
+            <div class="card mr-2 d-inline-block shadow-lg">
+              <div class="card-img-top">
+                <!-- <img src="../assets/info-bar-countries.svg" class="img-fluid border-radius p-4" alt="Member">  -->
+                <!-- <i class="fas fa-tachometer-alt" style="height: 200px; width: 200px;"></i> -->
+              </div>
+              <div class="card-body area-card">
+                <h3 class="card-title">Greater London</h3>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis consectetur quas, provident sit quibusdam similique,
+                  impedit aperiam in quidem ratione voluptatem
+                </p>
+                <a href="#" class="text-decoration-none">See More</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-12 text-center">
+            <div class="card mr-2 d-inline-block shadow-lg">
+              <div class="card-img-top">
+                <!-- <img src="../assets/info-bar-countries.svg" class="img-fluid border-radius p-4" alt="Member">  -->
+                <!-- <i class="fas fa-tachometer-alt" style="height: 200px; width: 200px;"></i> -->
+              </div>
+              <div class="card-body">
+                <h3 class="card-title">Kent</h3>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis consectetur quas, provident sit quibusdam similique,
+                  impedit aperiam in quidem ratione voluptatem
+                </p>
+                <a href="#" class="text-decoration-none">See More</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-12 text-center">
+            <div class="card mr-2 d-inline-block shadow-lg">
+              <div class="card-img-top">
+                <!-- <img src="../assets/info-bar-countries.svg" class="img-fluid border-radius p-4" alt="Member">  -->
+                <!-- <i class="fas fa-tachometer-alt" style="height: 200px; width: 200px;"></i> -->
+              </div>
+              <div class="card-body">
+                <h3 class="card-title">Hertfordshire</h3>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis consectetur quas, provident sit quibusdam similique,
+                  impedit aperiam in quidem ratione voluptatem
+                </p>
+                <a href="#" class="text-decoration-none">See More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
